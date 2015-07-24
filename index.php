f6041adafb8d767fee42691731f4b5903b4cdae5
SilRage <silrage23@mail.ru>
2015-07-24 14:46:53 +0300

    add to library less.js

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Dev version commercial offer</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet/less" href="less/styles.less">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+	<!-- Activate LESS CSS -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.3/less.min.js"></script>
+
+</head>
+<body>
+
+	<div id="CO_main_menu">
+		<nav>
+			<ul>
+				<li>ГК ГЛОНАССсофт</li>
+				<li>Платформа</li>
+				<li>Экономическая эффективность</li>
+				<li>Тахограф Атол Drive 5</li>
+			</ul>
+		</nav>
+	</div>
+
+</body>
+</html>
